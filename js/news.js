58631be562a6836bc666deda87b84397859125b2
djblanc360 <djblanc360@gmail.com>
2018-11-12 00:39:34 -0800

    masonry styling

--- js/news.js
+++ js/news.js
@@ -4,7 +4,7 @@ var $grid = $('.grid').masonry({
 });
 
 $(function() {
-  var elems = [ getItemElement(), getItemElement(), getItemElement() ];
+  var elems = [ getItemElement() ];
   // make jQuery object
   var $elems = $( elems );
   $grid.prepend( $elems ).masonry( 'prepended', $elems );
@@ -13,10 +13,9 @@ $(function() {
 // create <div class="grid-item"></div>
 function getItemElement() {
   var elem = document.createElement('div');
-  //var hRand = Math.random();
 
-  //var heightClass = hRand > 0.85 ? 'grid-item--height4' : hRand > 0.6 ? 'grid-item--height3' : hRand > 0.35 ? 'grid-item--height2' : '';
-  //elem.className = 'grid-item ' + ' ' + heightClass;
+
+
   elem.className = 'grid-item ';
   return elem;
 }
