be761f5b6d2f1454323d70f2e9715654a3b5d1d4
djblanc360 <djblanc360@gmail.com>
2018-11-12 00:32:54 -0800

    masonry integration

--- index.php
+++ index.php
@@ -21,68 +21,50 @@ get_header();
 		<main id="main" class="site-main">
 
 			<div class="container">
-				
+
 				<div class="grid">
-				  <div class="grid-item"></div>
-				  <div class="grid-item grid-item--width2 grid-item--height2"></div>
-				  <div class="grid-item grid-item--height3"></div>
-				  <div class="grid-item grid-item--height2"></div>
-				  <div class="grid-item grid-item--width3"></div>
-				  <div class="grid-item"></div>
-				  <div class="grid-item"></div>
-				  <div class="grid-item grid-item--height2"></div>
-				  <div class="grid-item grid-item--width2 grid-item--height3"></div>
-				  <div class="grid-item"></div>
-				  <div class="grid-item grid-item--height2"></div>
-				  <div class="grid-item"></div>
-				  <div class="grid-item grid-item--width2 grid-item--height2"></div>
-				  <div class="grid-item grid-item--width2"></div>
-				  <div class="grid-item"></div>
-				  <div class="grid-item grid-item--height2"></div>
-				  <div class="grid-item"></div>
-				  <div class="grid-item"></div>
-				  <div class="grid-item grid-item--height3"></div>
-				  <div class="grid-item grid-item--height2"></div>
-				  <div class="grid-item"></div>
-				  <div class="grid-item"></div>
-				  <div class="grid-item grid-item--height2"></div>
-				</div>
+					<div class="grid-item">
+
+						<?php
+						if ( have_posts() ) :
+
+							if ( is_home() && ! is_front_page() ) :
+								?>
+								<header>
+									<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+								</header>
+								<?php
+							endif;
+
+							/* Start the Loop */
+							while ( have_posts() ) :
+								the_post();
+
+								/*
+								 * Include the Post-Type-specific template for the content.
+								 * If you want to override this in a child theme, then include a file
+								 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+								 */
+								get_template_part( 'template-parts/content', get_post_type() );
+
+							endwhile;
+
+							the_posts_navigation();
+
+						else :
+
+							get_template_part( 'template-parts/content', 'none' );
+
+						endif;
+						?>						
+
+					</div><!--end grid-tem-->
+				</div><!--end grid -->
 
 				<div class="row">
 					<div class="col-sm-9">
 
-		<?php
-		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
 
 
 					</div>
--- js/news.js
+++ js/news.js
@@ -1,4 +1,22 @@
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: 160
+var $grid = $('.grid').masonry({
+  columnWidth: 10,
+  itemSelector: '.grid-item'
 });
+
+$(function() {
+  var elems = [ getItemElement(), getItemElement(), getItemElement() ];
+  // make jQuery object
+  var $elems = $( elems );
+  $grid.prepend( $elems ).masonry( 'prepended', $elems );
+});
+
+// create <div class="grid-item"></div>
+function getItemElement() {
+  var elem = document.createElement('div');
+  //var hRand = Math.random();
+
+  //var heightClass = hRand > 0.85 ? 'grid-item--height4' : hRand > 0.6 ? 'grid-item--height3' : hRand > 0.35 ? 'grid-item--height2' : '';
+  //elem.className = 'grid-item ' + ' ' + heightClass;
+  elem.className = 'grid-item ';
+  return elem;
+}
