de6ae4f939e14a980405424bd29a54e34c474d76
djblanc360 <djblanc360@gmail.com>
2018-11-12 22:40:14 -0800

    masonry styling

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ get_header();
 <div class="row" id="ms-container">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <div class="ms-item col-sm-6 col-sm-6 col-sm-6 col-xs-12">
+    <div class="ms-item col-lg-6 col-md-6 col-sm-6 col-xs-12">
 
         <?php if (has_post_thumbnail()) : ?>
 
