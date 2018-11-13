6e9094b7ef249d475a5836822106df89dba189a9
djblanc360 <djblanc360@gmail.com>
2018-11-12 22:07:41 -0800

    masonry styling

--- index.php
+++ index.php
@@ -1,81 +1,63 @@
-<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.js"></script>
-<script src="/path/to/masonry.pkgd.min.js"></script>
-<script src="/js/news.js"></script>
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package University_Lab_Partners
- */
+<?php get_header(); ?>
 
-get_header();
-?>
+<div class="row" id="ms-container">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-			<div class="container">
+    <div class="ms-item col-lg-6 col-md-6 col-sm-6 col-xs-12">
 
-				<div class="row">
+        <?php if (has_post_thumbnail()) : ?>
 
-					<div class="col-9">
+            <figure class="article-preview-image">
 
-						<div class="grid">
-							<div class="grid-item">
-							</div><!--end grid-tem-->
-						</div><!--end grid -->
-								<?php
-								if ( have_posts() ) :
+                <?php the_post_thumbnail('large'); ?>
 
-									if ( is_home() && ! is_front_page() ) :
-										?>
-										<header>
-											<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-										</header>
-										<?php
-									endif;
+            </figure>
 
-									/* Start the Loop */
-									while ( have_posts() ) :
+        <?php else : ?>
 
-										the_post();
+        <?php endif; ?>
 
-										/*
-										 * Include the Post-Type-specific template for the content.
-										 * If you want to override this in a child theme, then include a file
-										 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-										 */
-										get_template_part( 'template-parts/content', get_post_type() );
+            <h2 class="post-title"><a href="<?php the_permalink(); ?>" class="post-title-link"><?php the_title(); ?></a></h2>
 
-									endwhile;
+        <?php the_excerpt(); ?>
 
-									the_posts_navigation();
+    <div class="clearfix"></div>
 
-								else :
+<a href="<?php the_permalink(); ?>" class="btn btn-green btn-block">Read More</a>
 
-									get_template_part( 'template-parts/content', 'none' );
+    <div class="clearfix"></div>
 
-								endif;
-								?>
+    </div>
+
+    <?php endwhile;
+
+    else : ?>
+
+        <article class="no-posts">
+
+            <h1><?php _e('No posts were found.'); ?></h1>
+
+        </article>
+    <?php endif; ?>
+
+                </div>
+<div class="clearfix"></div>
 
 
 
-					</div> <!--end col-9 -->
-					<div class="col-3">
-						<?php get_sidebar(); ?>
-					</div>
-				</div>
-			</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    <script type="text/javascript">
 
-<?php
-get_footer();
+        jQuery(window).load(function() {
+      var container = document.querySelector('#ms-container');
+      var msnry = new Masonry( container, {
+        itemSelector: '.ms-item',
+        columnWidth: '.ms-item',
+      });
+
+        });
+
+
+    </script>
+<?php get_footer(); ?>
