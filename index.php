0b0921d7fe98e61107a836c4ae1fcd022e995409
djblanc360 <djblanc360@gmail.com>
2018-11-13 11:29:42 -0800

    hover overlay

--- index.php
+++ index.php
@@ -34,7 +34,10 @@ get_header();
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
     <div class="ms-item col-lg-6 col-md-6 col-sm-6 col-xs-12">
-
+			<!-- ADD OVERLAY HERE -->
+			<div class="overlay">
+				<span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</span>
+	</div><!-- end overlay -->		
         <?php if (has_post_thumbnail()) : ?>
 
             <figure class="article-preview-image">
@@ -61,10 +64,7 @@ get_header();
 				</p>
     <div class="clearfix"></div>
 
-				<!-- ADD OVERLAY HERE -->
-				<div class="overlay">
-					<span>This is some text I want to display</span>
-				</div>
+
     </div>
 
     <?php endwhile;
