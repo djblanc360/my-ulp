9b8dd84b3ec5f7c88fb67a6c6ac3a24b51482404
djblanc360 <djblanc360@gmail.com>
2018-11-12 22:24:21 -0800

    masonry styling

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package University_Lab_Partners
+ */
+
+get_header();
+?>
 <script type="text/javascript">
 
 		jQuery(window).load(function() {
@@ -12,8 +27,10 @@
 
 
 </script>
-<div class="row" id="ms-container">
 
+
+<div class="row" id="ms-container">
+<h1>The latest news from ULP</h1>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
     <div class="ms-item col-lg-6 col-md-6 col-sm-6 col-xs-12">
