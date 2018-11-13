be6990815d3bafb492d71cf2fdb1a741e81c896d
djblanc360 <djblanc360@gmail.com>
2018-11-12 22:16:26 -0800

    masonry styling

--- index.php
+++ index.php
@@ -14,58 +14,51 @@
 
 get_header();
 ?>
-<div id="primary" class="content-area">
-	<main id="main" class="site-main">
-
-		<div class="container">
 
 <div class="row" id="ms-container">
 
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		    <div class="ms-item col-lg-6 col-md-6 col-sm-6 col-xs-12">
+    <div class="ms-item col-lg-6 col-md-6 col-sm-6 col-xs-12">
 
-		        <?php if (has_post_thumbnail()) : ?>
+        <?php if (has_post_thumbnail()) : ?>
 
-		            <figure class="article-preview-image">
+            <figure class="article-preview-image">
 
-		                <?php the_post_thumbnail('large'); ?>
+                <?php the_post_thumbnail('large'); ?>
 
-		            </figure>
+            </figure>
 
-		        <?php else : ?>
+        <?php else : ?>
 
-		        <?php endif; ?>
+        <?php endif; ?>
 
-		            <h2 class="post-title"><a href="<?php the_permalink(); ?>" class="post-title-link"><?php the_title(); ?></a></h2>
+            <h2 class="post-title"><a href="<?php the_permalink(); ?>" class="post-title-link"><?php the_title(); ?></a></h2>
 
-		        <?php the_excerpt(); ?>
+        <?php the_excerpt(); ?>
 
-		    <div class="clearfix"></div>
+    <div class="clearfix"></div>
 
-		<a href="<?php the_permalink(); ?>" class="btn btn-green btn-block">Read More</a>
+<a href="<?php the_permalink(); ?>" class="btn btn-green btn-block">Read More</a>
 
-		    <div class="clearfix"></div>
+    <div class="clearfix"></div>
 
-		    </div>
+    </div>
 
-		    <?php endwhile;
+    <?php endwhile;
 
-		    else : ?>
+    else : ?>
 
-		        <article class="no-posts">
+        <article class="no-posts">
 
-		            <h1><?php _e('No posts were found.'); ?></h1>
+            <h1><?php _e('No posts were found.'); ?></h1>
 
-		        </article>
-		    <?php endif; ?>
+        </article>
+    <?php endif; ?>
 
-		 </div>
-		<div class="clearfix"></div>
-</div>
+                </div>
+<div class="clearfix"></div>
 
-</main><!-- #main -->
-</div><!-- #primary -->
 
 
 
