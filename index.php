f515f733fb6694712cf3add5404ceb3afd305892
djblanc360 <djblanc360@gmail.com>
2018-11-14 16:06:40 -0800

    fix pagination

--- index.php
+++ index.php
@@ -32,13 +32,10 @@ get_header();
 <div class="container">
 <div class="row" id="ms-container">
 
-	<?php
-	   // the query
-	   $the_query = new WP_Query( array(
-	      'posts_per_page' => 9,
-	   ));
-	?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<?php $the_query = new WP_Query( 'posts_per_page=9' ); ?>
+
+<?php if ($the_query->have_posts()) : while ($the_query->have_posts()) : the_post(); ?>
 	<!-- ADD OVERLAY HERE -->
 
     <div class="ms-item col-lg-6 col-md-6 col-sm-6 col-xs-12">
